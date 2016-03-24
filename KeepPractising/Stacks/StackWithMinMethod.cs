ac2e212b92e29dbac877e0f5fc03b4ed10e1323f
Harsh <hars@outlook.com>
2016-03-24 17:49:00 +0530

    A custom stack which also supports GetMin method.

---
+++ KeepPractising/Stacks/StackWithMinMethod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace KeepPractising.Stacks
+{
+    class StackWithMinMethod<T> : MyStack<T> where T : IComparable<T>, IEquatable<T>
+    {
+        MyStack<T> minStack;
+
+        public StackWithMinMethod()
+        {
+            minStack = new MyStack<T>();
+        }
+
+        public new void Push(T obj)
+        {
+            base.Push(obj);
+
+            if (minStack.Length == 0)
+                minStack.Push(obj);
+            else if (obj.CompareTo(minStack.Peek()) < 0)
+                minStack.Push(obj);
+        }
+
+        public new T Pop()
+        {
+            var temp = base.Pop();
+            if (minStack.Peek().Equals(temp))
+                minStack.Pop();
+
+            return temp;
+        }
+
+        public T GetMin()
+        {
+            return minStack.Peek();
+        }
+    }
+}
